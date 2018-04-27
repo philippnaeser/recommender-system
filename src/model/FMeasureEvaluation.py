d9f2095b3d60f552b5a0ff900753668da4810494
andreeaiana <37438455+andreeaiana@users.noreply.github.com>
2018-04-27 10:58:43 +0200

    Computes the F-beta measure of the model

---
+++ src/model/FMeasureEvaluation.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 27 10:12:50 2018
+
+@author: Andreea
+"""
+import numpy as np
+
+from AbstractClasses import AbstractEvaluation
+from RecallEvaluation import RecallEvaluation
+from PrecisionEvaluation import PrecisionEvaluation
+
+class FMeasureEvaluation(AbstractEvaluation):
+    
+    """
+        Computes the F-beta measure of the model.
+        
+        Args:
+            recommendation (list): The list of recommendations returned 
+                                    by the model.
+            truth (list): The test set list of conferences attended
+            beta (int): Parameter which indicates how many times is recall
+                        considered more important than precision
+        
+        Returns:
+            int: F-beta score 
+            
+    """
+    
+    def evaluate(self, recommendation, truth, beta):
+        
+        recall = RecallEvaluation().evaluate(recommendation, truth) 
+        precision = PrecisionEvaluation().evaluate(recommendation, truth)
+        
+        if ((precision!=0) or (recall!=0)):
+            measure = (1 + beta)*(precision*recall)/(np.square(beta)*precision + recall)
+        else:
+            measure = 0
+        
+        print("F{} score = {}".format(beta, measure))
+        return measure
