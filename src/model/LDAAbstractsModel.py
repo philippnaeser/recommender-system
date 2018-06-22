05730b3a719b9d954ad2b7f13eaed8c7d110e0a2
andreeaiana <andreeaiana@gmail.com>
2018-06-22 12:49:20 +0200

    LDA (Latent Dirichlet Allocation) model for abstracts: work in progress

---
+++ src/model/LDAAbstractsModel.py
@@ -0,0 +1,190 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 20 14:17:57 2018
+
+@author: Andreea
+"""
+
+
+from AbstractClasses import AbstractModel 
+import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
+from nltk.stem.porter import PorterStemmer # use the stemmer from nltk
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.decomposition import LatentDirichletAllocation
+import re
+import os
+import pickle
+
+class LDAAbstractsModel(AbstractModel):
+    
+    persistent_file_x = os.path.join("..","..","data","processed","abstracts.lda.model.X.pkl")
+    persistent_file_factors = os.path.join("..","..","data","processed","abstracts.lda.model.factors.pkl")
+    
+    ##########################################
+    def __init__(self,recs=10):
+        self.stemmer = PorterStemmer()
+        self.token_pattern = re.compile(r"(?u)\b\w\w+\b")
+        self.stem_vectorizer = TfidfVectorizer(
+                tokenizer=self
+                ,stop_words="english"
+                #,min_df=0.05
+                #,max_df=0.8
+        )
+        # number of recommendations to return
+        self.recs = recs
+    
+    ##########################################
+    def query_single(self,abstract):
+        """
+        Queries the model and returns a list of recommendations.
+        
+        Args:
+            abstract (str): The abstract as a string.
+        
+        Returns:
+            str[]: name of the conference
+            double[]: confidence scores
+        """
+        
+        q_v = (self.stem_vectorizer.transform([abstract]))
+        transformed_q_v = (self.lda.transform(q_v))
+        
+        # normalize
+        row_sums = transformed_q_v.sum(axis=1)
+        row_sums = np.where(row_sums == 0, 0.00000001, row_sums)
+        transformed_q_v = transformed_q_v / row_sums[:, np.newaxis]
+        
+        sim = cosine_similarity(transformed_q_v,self.transformed_matrix)[0]
+        o = np.argsort(-sim)
+        print(self.data.iloc[o[0]].chapter_abstract)
+        return [
+                list(self.data.iloc[o][0:self.recs].conference_name),
+                sim[o][0:self.recs]
+                ]
+    
+    ##########################################
+    def query_batch(self,batch):
+        """
+        Queries the model and returns a list of recommendations for each request.
+        
+        Args:
+            batch[str]: The list of abstracts.
+        
+        Returns:
+            A list of size 'len(batch)' which contains the recommendations for each item of the batch.
+            If author not found, the value is None.
+            
+            str[]: name of the conference
+            double[]: confidence scores
+        """
+        
+        #print(batch)
+        q_v = (self.stem_vectorizer.transform(batch))
+        transformed_q_v = (self.lda.transform(q_v))
+        print("Abstracts transformed.")
+        print(q_v.shape)
+        print(transformed_q_v.shape)
+        
+        # normalize
+        row_sums = transformed_q_v.sum(axis=1)
+        row_sums = np.where(row_sums == 0, 0.00000001, row_sums)
+        transformed_q_v = transformed_q_v / row_sums[:, np.newaxis]
+
+        sim = cosine_similarity(transformed_q_v, self.transformed_matrix)
+        print("Cosine similarity computed.")
+        o = np.argsort(-np.array(sim))
+        
+        conference = list()
+        confidence = list()
+        index = 0
+        self.count_init(len(o))
+        for order in o:
+            conference.append(
+                    list(self.data.iloc[order][0:self.recs].conference_name)
+            )
+            confidence.append(
+                    sim[index][order][0:self.recs]
+            )
+            index += 1
+            self.count()
+        
+            
+        return [conference,confidence]
+    
+   ##########################################
+    def train(self, data, dimensions):
+        if not self._load_model_x():
+            print("Stem matrix not persistent yet. Creating now.")
+            for check in ["chapter_abstract","conference","conference_name"]:
+                if not check in data.columns:
+                    raise IndexError("Column '{}' not contained in given DataFrame.".format(check))
+            
+            self.data = data
+            self.stem_matrix = self.stem_vectorizer.fit_transform(data.chapter_abstract)
+            self._save_model_x()
+            
+        if not self._load_model_factors():
+            print("LDA not persistent yet. Creating now.")
+            self.dimensions = dimensions
+            self.lda = LatentDirichletAllocation(
+                    n_components=self.dimensions
+                    , learning_method = 'online'
+                    , random_state=0)
+            self.lda.fit(self.stem_matrix)
+            self.transformed_matrix = self.lda.transform(self.stem_matrix)
+            
+            # normalize the transformed matrix
+            row_sums = self.transformed_matrix.sum(axis=1)
+            row_sums = np.where(row_sums == 0, 0.00000001, row_sums)
+            self.transformed_matrix = self.transformed_matrix / row_sums[:, np.newaxis]
+            
+            
+            self._save_model_factors()
+        
+    ##########################################
+    def __call__(self, doc):
+        # tokenize the input with a regex and stem each token
+        return [self.stemmer.stem(t) for t in self.token_pattern.findall(doc)]
+    
+    ##########################################
+    def _get_word_freq(self, matrix, vectorizer):
+        '''Function for generating a list of (freq, word)'''
+        return sorted([(matrix.getcol(idx).sum(), word) for word, idx in vectorizer.vocabulary_.items()], reverse=True)
+    
+    ##########################################
+    def _save_model_x(self):
+        with open(LDAAbstractsModel.persistent_file_x,"wb") as f:
+            pickle.dump([self.stem_matrix, self.stem_vectorizer, self.data], f)
+            
+    ##########################################
+    def _save_model_factors(self):
+        with open(LDAAbstractsModel.persistent_file_factors,"wb") as f:
+            pickle.dump([self.lda, self.transformed_matrix], f)
+    
+    ##########################################
+    def _load_model_x(self):
+        if os.path.isfile(LDAAbstractsModel.persistent_file_x):
+            print("Loading persistent models: X")
+            with open(LDAAbstractsModel.persistent_file_x,"rb") as f:
+                self.stem_matrix, self.stem_vectorizer, self.data = pickle.load(f)
+                print("Loaded.")
+                return True
+        
+        return False
+    
+    ##########################################
+    def _load_model_factors(self):
+        if os.path.isfile(LDAAbstractsModel.persistent_file_factors):
+            print("Loading persistent models: Factors")
+            with open(LDAAbstractsModel.persistent_file_factors,"rb") as f:
+                self.lda, self.transformed_matrix = pickle.load(f)
+                print("Loaded.")
+                return True
+        
+        return False
+    
+    ##########################################
+    def print_top_k(self, k):
+        for tfidf, word in self._get_word_freq(self.stem_matrix, self.stem_vectorizer)[:k]:
+            print("{:.3f} {}".format(tfidf, word))
---
+++ src/model/LDAAbstractsModelEvaluation.py
@@ -0,0 +1,140 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 20 14:18:11 2018
+
+@author: Andreea
+"""
+
+import os
+import sys
+
+
+# query batchifier to avoid OutOfMemory exceptions
+class Batchifier():
+    def __init__(self):
+        self.recv, self.send_c = Pipe()
+        self.recv_c, self.send = Pipe()
+
+    def run(self,batch):
+        # return model.query_batch(batch)
+
+        p = Process(target=self.f, args=(self.recv_c, self.send_c))
+        p.start()
+        
+        self.send.send(batch)
+        self.send.close()
+        
+        results = self.recv.recv()
+        
+        p.join()
+        
+        return results
+
+    def f(self, recv, send):
+        sys.stderr = open("debug-multiprocessing.err.txt", "w")
+        sys.stdout = open("debug-multiprocessing.out.txt", "w")
+        sys.path.insert(0, os.getcwd())
+        sys.path.insert(0, os.path.join(os.getcwd(),"..","data"))
+        
+        from LDAAbstractsModel import LDAAbstractsModel
+        model = LDAAbstractsModel()
+        model._load_model_x()
+        model._load_model_factors()
+        
+        batch = recv.recv()
+        results = model.query_batch(batch)
+        
+        send.send(results)
+        send.close()
+
+if __name__ == '__main__':
+    sys.path.insert(0, os.path.join(os.getcwd(),"..","data"))
+    
+    from LDAAbstractsModel import LDAAbstractsModel
+    from DataLoader import DataLoader
+    import pandas as pd
+    import numpy as np
+    from multiprocessing import Process, Pipe
+    
+    ### load training data if it is already pickled, otherwise create it from scratch
+    
+    filename = "abstracts.lda.train.pkl"
+    
+    d_train = DataLoader()
+    if not d_train.get_persistent(filename):
+        d_train.papers(["2013","2014","2015"]).abstracts().conferences()
+        d_train.data = d_train.data[["chapter","chapter_abstract","conference","conference_name"]].copy()
+        d_train.data.drop(
+            list(d_train.data[pd.isnull(d_train.data.chapter_abstract)].index),
+            inplace=True
+        )
+        d_train.make_persistent(filename)
+    
+    model = LDAAbstractsModel()
+    dimensions = 500
+    model.train(d_train.data, dimensions)
+     
+    ### load test data if it is already pickled, otherwise create it from scratch
+    
+    filename = "abstracts.lda.test.pkl"
+    
+    d_test = DataLoader()
+    if not d_test.get_persistent(filename):
+        d_test.papers(["2016"]).abstracts().conferences()
+        d_test.data = d_test.data[["chapter","chapter_abstract","conference","conference_name"]].copy()
+        d_test.data.drop(
+            list(d_test.data[pd.isnull(d_test.data.chapter_abstract)].index),
+            inplace=True
+        )
+        d_test.make_persistent(filename)
+
+    ### create test query and truth values
+
+    query_test = list(d_test.data.chapter_abstract)#[0:1000]
+    
+    conferences_truth = list()
+    confidences_truth = list()
+    
+    for conference in list(d_test.data.conference_name):
+        conferences_truth.append([conference])
+        confidences_truth.append([1])
+        
+    truth = [conferences_truth,confidences_truth]
+        
+    ### apply test query and retrieve results
+    
+    batchsize = 200
+    minibatches = np.arange(0,len(query_test),batchsize)
+    
+    conferences = list()
+    confidences = list()
+    
+    # batchify the query to avoid OutOfMemory exceptions
+    for i in minibatches:
+        minibatch = query_test[i:(i+batchsize)]
+        batchifier = Batchifier()
+        print("Running minibatch [{}/{}]".format(int((i/batchsize)+1),len(minibatches)))
+        results = batchifier.run(minibatch)
+        conferences.extend(results[0])
+        confidences.extend(results[1])
+        
+    recommendation = [conferences,confidences]
+        
+    ### evaluate
+    
+    print("Computing MAP.")
+    from MAPEvaluation import MAPEvaluation
+    evaluation = MAPEvaluation()
+    ev_map = evaluation.evaluate(recommendation, truth)
+    
+    print("Computing Recall.")
+    from MeanRecallEvaluation import MeanRecallEvaluation
+    evaluation = MeanRecallEvaluation()
+    ev_recall = evaluation.evaluate(recommendation, truth)
+    
+    print("Computing Precision.")
+    from MeanPrecisionEvaluation import MeanPrecisionEvaluation
+    evaluation = MeanPrecisionEvaluation()
+    ev_precision = evaluation.evaluate(recommendation, truth)
+    
+    print("Recall: {}, Precision: {}, MAP: {}".format(ev_recall,ev_precision,ev_map))
