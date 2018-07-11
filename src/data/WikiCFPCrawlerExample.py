a36453bfd1b4ee46485b6508d91b2b55bf0c299c
andreeaiana <andreeaiana@gmail.com>
2018-07-11 21:49:19 +0200

    Crawled WikiCFP conferences with new crawler

--- src/data/WikiCFPCrawlerExample.py
+++ src/data/WikiCFPCrawlerExample.py
@@ -12,8 +12,8 @@ import os
 crawler = WikiCFPCrawler.WikiCFPCrawler()
 
 ##Select the range of conferences (event ids) to be crawled 
-start_eventid = 0
-end_eventid = 100000
+start_eventid = 0 #last crawled ID: 78661
+end_eventid = 79000 
 
 ##Crawl conferences for the chosen event ids
 all_conferences = crawler.crawl_conferences(start_eventid, end_eventid)
@@ -26,11 +26,12 @@ filename_all_conferences = os.path.join(
 all_conferences.to_csv(filename_all_conferences)
 
 ##Parse the crawled data
-parser = WikiCFPDataParser.WikiCFPDataParser(all_conferences)
+#parser = WikiCFPDataParser.WikiCFPDataParser(all_conferences)
 
-##Create pickle files for the upcoming conferences
-conferences_2016 = parser.getConferencesPerYear('2016')
-conferences_2017 = parser.getConferencesPerYear('2017')
-conferences_2018 = parser.getConferencesPerYear('2018')
+##Create pickle files for conferences in a given year
+#conferences_2016 = parser.getConferencesPerYear('2016')
+#conferences_2017 = parser.getConferencesPerYear('2017')
+#conferences_2018 = parser.getConferencesPerYear('2018')
 
-conferences_2016_2017 = parser.getConferencePerPeriod('2016', '2017')
+##Create pickle files for conferences in a given period
+#conferences_2016_2017 = parser.getConferencePerPeriod('2016', '2017')
