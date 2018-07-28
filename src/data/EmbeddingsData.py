e21cef371352e45cabc5b71e91cd1d1df1b76d5c
andreeaiana <andreeaiana@gmail.com>
2018-07-28 12:50:08 +0200

    Changed to use TimerCounter class from neuralnets

--- src/data/EmbeddingsData.py
+++ src/data/EmbeddingsData.py
@@ -10,46 +10,16 @@ import os
 
 sys.path.insert(0, os.path.join(os.getcwd()))
 sys.path.insert(0, os.path.join(os.getcwd(),".."))
-sys.path.insert(0, os.path.join(os.getcwd(),"..","..","model"))
-sys.path.insert(0, os.path.join(os.getcwd(),"..","..", "..", "neuralnets"))
+sys.path.insert(0, os.path.join(os.getcwd(),"..", "model"))
+sys.path.insert(0, os.path.join(os.getcwd(),"..", "..", "neuralnets"))
 
 from DataLoader import DataLoader as SciGraphLoader
+from TimerCounter import Timer
 from nltk.tokenize import sent_tokenize
 import pandas as pd
-import time
 import gzip
 import gensim, logging
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
-
-class Timer:
-    start_time = []
-    
-    ### start runtime check
-    def tic(self):
-        self.start_time.append(time.time())
-    
-    ### print runtime information
-    def toc(self):
-        diff = (time.time() - self.start_time.pop())
-        print("Timer :: toc --- %s seconds ---" % diff)
-        return diff
-        
-    def set_counter(self,c,max=100):
-        self.counter_max = c
-        self.counter = 0
-        self.checkpoint = int(self.counter_max/max)
-        self.step = self.checkpoint
-        self.tic()
-        
-    def count(self,add=1):
-        self.counter = self.counter + add
-        
-        if (self.counter >= self.checkpoint):
-            print("Timer :: Checkpoint reached: {}%".format(int(self.counter*100/self.counter_max)))
-            self.toc()
-            self.checkpoint += self.step
-            if self.checkpoint <= self.counter_max:
-                self.tic()
 
 #### TRAINING PARAMETERS
 
