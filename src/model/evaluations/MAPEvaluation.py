edf37c2e3765a0a99e2229abcb8fb83c7fc7ff0c
andreeaiana <andreeaiana@gmail.com>
2018-08-06 18:31:42 +0200

    MAPEvaluation: duplicates are ignored in the calculation

--- src/model/evaluations/MAPEvaluation.py
+++ src/model/evaluations/MAPEvaluation.py
@@ -6,7 +6,6 @@ Created on Thu May 10 23:14:36 2018
 """
 
 from AbstractClasses import AbstractEvaluation
-import numpy as np
 
 class MAPEvaluation(AbstractEvaluation):
     
@@ -22,39 +21,32 @@ class MAPEvaluation(AbstractEvaluation):
         Returns:
             int: the mean of all average precision scores for all the queries
             
-        """
+        """ 
         sumAveragePrecisions = 0
         rank = 0
         
-        count = len(recommendation[0])
-        checkpoint = max(int(count/20),1)
-        
         #Sum the average precisions@k for all queries
         for i in range(len(recommendation[0])):
-            
-            if (i%checkpoint)==0:
-                print("Computing average precision: {}%".format(int(i/count*100)))
-            
+           
             sumPrecisions = 0
             averagePrecision = 0
               
             if truth[0][i] is not None:
                 if recommendation[0][i] is not None: 
-                    #Drop duplicates from the recommendation list
-                    unique_recommendation, indices = np.unique(recommendation[0][i], return_index = True)
-                    unique_recommendation = unique_recommendation[np.argsort(indices)]
+                    checked = set()
                     
-                    for j in range(len(unique_recommendation)):
-                        if (unique_recommendation[j] is not None) and (unique_recommendation[j] in truth[0][i]):
+                    for j in range(len(recommendation[0][i])):
+                        if (recommendation[0][i][j] is not None) and (recommendation[0][i][j] in truth[0][i]) and not(recommendation[0][i][j] in checked):
                            rank = j+1
-                           sumPrecisions = sumPrecisions + self._precisionAtK(unique_recommendation, truth[0][i], rank)
-                    
+                           sumPrecisions = sumPrecisions + self._precisionAtK(recommendation[0][i], truth[0][i], rank)
+                           checked.add(recommendation[0][i][j])
+                           
                     averagePrecision = sumPrecisions/len(truth[0][i])
             sumAveragePrecisions += averagePrecision
 
         measure = sumAveragePrecisions/len(recommendation[0])
         
-        print("Mean average precision = {}".format(measure))
+        #print("Mean average precision = {}".format(measure))
         return measure
     
     
@@ -66,7 +58,7 @@ class MAPEvaluation(AbstractEvaluation):
             rank = i+1
             if (recommendation[i] in truth) and (rank<=k):
                 countRelevantRetrieved += 1
-            
+
         if k!= 0:
             measure = countRelevantRetrieved/k
         else:
