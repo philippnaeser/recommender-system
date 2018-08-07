64fc53931eda6bbb204591983b75e7e26209fd41
steffen-jung <33086982+steffen-jung@users.noreply.github.com>
2018-08-08 01:11:18 +0200

    MAPkEvaluation: fixed bug with wrong measures if recommendations contain None.

--- src/model/evaluations/MAPkEvaluation.py
+++ src/model/evaluations/MAPkEvaluation.py
@@ -20,37 +20,37 @@ class MAPkEvaluation(AbstractEvaluation):
     #########################################################
     def evaluate_no_duplicates(self,recommendation,truth):
         map_sum = 0
-        try:
-            for i, q in enumerate(recommendation[0][0:self.k]):
-                hits = 0
-                ap = 0
-                denom = max(len(truth[0][i]),self.k)
-                for rank, rec in enumerate(q,1):
+        for i, q in enumerate(recommendation[0]):
+            hits = 0
+            ap = 0
+            denom = max(len(truth[0][i]),self.k)
+            try:
+                for rank, rec in enumerate(q[0:self.k],1):
                     add = rec in truth[0][i]
                     hits += add
                     ap += add*(hits/rank)
                 map_sum += ap/denom
-        except TypeError:
-            pass # equivalent to adding 0 to map_sum if recommendation is None
+            except TypeError:
+                pass # equivalent to adding 0 to map_sum if recommendation is None
         return map_sum/len(recommendation[0])
     
     #########################################################
     def evaluate_duplicates(self,recommendation,truth):
         map_sum = 0
-        try:
-            for i, q in enumerate(recommendation[0]):
-                hits = 0
-                ap = 0
-                denom = min(len(truth[0][i]),self.k)
-                hitlist = []
+        for i, q in enumerate(recommendation[0]):
+            hits = 0
+            ap = 0
+            denom = min(len(truth[0][i]),self.k)
+            hitlist = []
+            try:
                 for rank, rec in enumerate(q[0:self.k],1):
                     add = rec in truth[0][i]
                     hits += add
                     add = add & (rec not in hitlist)
                     if add: hitlist.append(rec)
                     ap += add*(hits/rank)
-                #print(ap/denom)
+                #print("{} = {}".format(rec,ap/denom))
                 map_sum += ap/denom
-        except TypeError:
-            pass # equivalent to adding 0 to map_sum if recommendation is None
+            except TypeError:
+                pass # equivalent to adding 0 to map_sum if recommendation is None
         return map_sum/len(recommendation[0])
