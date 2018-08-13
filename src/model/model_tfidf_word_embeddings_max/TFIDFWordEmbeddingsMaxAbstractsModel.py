6b0d3d5638cb170b55ef9d6f7408e306b716e685
andreeaiana <andreeaiana@gmail.com>
2018-08-14 01:30:40 +0200

    TFIDF-weighted Max Word Embeddings model for abstracts

---
+++ src/model/model_tfidf_word_embeddings_max/TFIDFWordEmbeddingsMaxAbstractsModel.py
@@ -0,0 +1,203 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 14 00:55:37 2018
+
+@author: Andreea
+"""
+
+from AbstractClasses import AbstractModel 
+from EmbeddingsParser import EmbeddingsParser
+from nltk.corpus import stopwords
+import numpy as np
+import os
+import pickle
+from scipy.spatial.distance import cdist
+
+
+class TFIDFWordEmbeddingsMaxAbstractsModel(AbstractModel):
+    
+    ##########################################
+    def __init__(self, embedding_model, pretrained = True, recs=10):
+        self.stopList = stopwords.words('english') 
+        self.embedding_model = embedding_model
+        self.pretrained = pretrained
+        
+        self.parser = EmbeddingsParser()
+        self.parser.load_model(self.embedding_model, self.pretrained)
+                
+        # number of recommendations to return
+        self.recs = recs
+    
+        description_embeddings = "-".join([
+                str(pretrained),
+                str(self.embedding_model),
+                "{}"
+        ])
+    
+        self.path = os.path.join("..","..","..","data","processed","model_tfidfwordembeddings_max")
+        if not os.path.isdir(self.path):
+            os.mkdir(self.path)
+        
+        self.persistent_file_x = os.path.join(self.path,
+                                              "model-X.pkl")
+        
+        self.persistent_file_embeddings = os.path.join(self.path,
+                                               "model-"+description_embeddings+"-Embeddings.pkl")
+
+    ##########################################
+    def query_single(self, abstract):
+        """
+        Queries the model and returns a list of recommendations.
+        
+        Args:
+            abstract (string): The abstract as a string.
+        
+        Returns:
+            str[]: name of the conference
+            double[]: confidence scores
+        """
+        return self.query_batch([abstract])
+
+    ##########################################
+    def query_batch(self,batch):
+        """
+        Queries the model and returns a list of recommendations for each request.
+        
+        Args:
+            batch[str]: The list of abstracts.
+        
+        Returns:
+            A list of size 'len(batch)' which contains the recommendations for each item of the batch.
+            If author not found, the value is None.
+            
+            str[]: name of the conference
+            double[]: confidence scores
+        """
+        conferences = list()
+        confidences = list()
+        #self.count_init(len(batch))
+        
+        q_v = self.parser.transform_tfidf_avg_vectors(
+                self._remove_stopwords(batch),
+                self.tfidf_weights
+                )
+        transformed_q_v = np.asarray(q_v)
+        #print("Abstracts transformed.")
+        #print("Dimensionality of batch: {}".format(transformed_q_v.shape))
+        
+        sim = 1-cdist(transformed_q_v, self.embedded_matrix, "cosine")
+        #print("Cosine similarity computed.")
+        o = np.argsort(-sim)
+
+        for index, order in enumerate(o):
+            data_conf = np.array(self.data["conferenceseries"])[order]
+            data_sim = np.array(sim[index])[order]
+            
+            conference = list()
+            confidence = list()
+            i = 0
+            while len(conference) < self.recs:
+                c = data_conf[i]
+                if c not in conference:
+                    conference.append(c)
+                    confidence.append(data_sim[i])   
+                i += 1
+                
+            conferences.append(
+                    conference
+            )
+            confidences.append(
+                    confidence
+            )
+            #self.count()
+            
+        return [conferences,confidences]
+    
+   ##########################################
+    def train(self, data, data_name):
+        if not self._load_model_x(data_name):
+            print("Transformed data not persistent yet. Transforming now.")
+            for check in ["chapter_abstract", "conferenceseries"]:
+                if not check in data.columns:
+                    raise IndexError("Column '{}' not contained in given DataFrame.".format(check))
+
+            data.chapter_abstract = self._remove_stopwords(data.chapter_abstract)
+            self.data = data
+            self._save_model_x(data_name)
+        else:
+           if len(self.data) != len(data):
+               raise ValueError("Mismatch vs. persistent training data size: Loaded: {} <-> Given: {}".format(len(self.data),len(data)))
+
+        if not self._load_model_embeddings(data_name):
+            print("Embeddings not persistent yet. Creating now.")
+            self.tfidf_weights = self.parser.compute_tfidf_weights(
+                                    self.data.chapter_abstract
+                                    )
+            self.embedded_matrix = self.parser.transform_tfidf_avg_vectors(
+                    self.data.chapter_abstract,
+                    self.tfidf_weights
+                    )
+            self.embedded_matrix = np.asarray(self.embedded_matrix)
+            self._save_model_embeddings(data_name)
+
+    ##########################################
+    def _remove_stopwords(self, text):
+        transformed_text = list()
+        for sentence in text:
+            content = " ".join([w for w in sentence.split() if w.lower() not in self.stopList])
+            transformed_text.append(content)
+        return transformed_text
+    
+    ##########################################
+    def _file_x(self,data_name):
+        return self.persistent_file_x.format(data_name)
+    
+    ##########################################
+    def _file_embeddings(self,data_name):
+        return self.persistent_file_embeddings.format(data_name)
+    
+    ##########################################
+    def _save_model_x(self,data_name):
+        file = self._file_x(data_name)
+        with open(file,"wb") as f:
+            pickle.dump([self.stopList, self.data], f)
+            
+    ##########################################
+    def _save_model_embeddings(self,data_name):
+        file = self._file_embeddings(data_name)
+        with open(file,"wb") as f:
+            pickle.dump([self.tfidf_weights, self.embedded_matrix], f)
+    
+    ##########################################
+    def _load_model_x(self,data_name):
+        file = self._file_x(data_name)
+        if os.path.isfile(file):
+            print("Loading persistent models: X")
+            with open(file,"rb") as f:
+                self.stopList, self.data = pickle.load(f)
+                print("Loaded.")
+                return True
+        
+        return False
+    
+    ##########################################
+    def _load_model_embeddings(self,data_name):
+        file = self._file_embeddings(data_name)
+        if os.path.isfile(file):
+            print("Loading persistent models: Embeddings")
+            with open(file,"rb") as f:
+                self.tfidf_weights, self.embedded_matrix = pickle.load(f)
+                print("Loaded.")
+                return True
+        
+        return False
+    
+    ##########################################
+    def _load_model(self,data_name):
+        return self._load_model_x(data_name) & self._load_model_embeddings(data_name)
+    
+    ##########################################
+    def _has_persistent_model(self,data_name):
+        if os.path.isfile(self._file_x(data_name)) and os.path.isfile(self._file_embeddings(data_name)):
+            return True
+        return False
---
+++ src/model/model_tfidf_word_embeddings_max/TFIDFWordEmbeddingsMaxAbstractsModelEvaluation.py
@@ -0,0 +1,124 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 14 00:56:25 2018
+
+@author: Andreea
+"""
+
+###### Script parameters #######
+
+EMBEDDING_MODEL = "w2v_100d_w10_SG_NS"
+PRETRAINED = False
+
+MAX_RECS = 10
+
+TRAINING_DATA = "small"
+TEST_DATA = "small"
+
+BATCHSIZE_EVALUATION = 200
+PROCESSES_EVALUATION = 3
+
+#################################
+
+import sys
+import os
+import multiprocessing as mp
+
+sys.path.insert(0, os.path.join(os.getcwd(),".."))
+sys.path.insert(0, os.path.join(os.getcwd(),"..","..","data"))
+sys.path.insert(0, os.path.join(os.getcwd(),"..","evaluations"))
+from TFIDFWordEmbeddingsMaxAbstractsModel import TFIDFWordEmbeddingsMaxAbstractsModel
+
+# Generate model (main + child process).
+
+model = TFIDFWordEmbeddingsMaxAbstractsModel(
+            embedding_model = EMBEDDING_MODEL,
+            pretrained = PRETRAINED,
+            recs=MAX_RECS
+    )
+
+# Method to run in a multiprocessing process.
+
+def evaluate_model(batch):
+    result = model.query_batch(batch)
+    return result
+
+# Load model in child process.
+
+if __name__ != '__main__':
+    #sys.stderr = open("debug-multiprocessing.err."+str(os.getppid())+".txt", "w")
+    #sys.stdout = open("debug-multiprocessing.out."+str(os.getppid())+".txt", "w")
+    model._load_model(TRAINING_DATA)
+
+# Main script.
+
+if __name__ == '__main__':
+    from DataLoader import DataLoader
+    import numpy as np
+    import time
+
+    # Train model if needed.
+    
+    if not model._has_persistent_model(TRAINING_DATA):
+        d_train = DataLoader()
+        d_train.training_data_for_abstracts(TRAINING_DATA)
+        model.train(d_train.data,TRAINING_DATA)
+
+    # Generate test query and truth values.
+
+    d_test = DataLoader()
+    query_test, truth = d_test.evaluation_data_for_abstracts(TEST_DATA)
+   
+   # Apply test query and retrieve results.
+    
+    minibatches = np.array_split(query_test,int(len(query_test)/BATCHSIZE_EVALUATION))
+    
+    conferences = list()
+    confidences = list()
+    
+    # Batchify the query to avoid OutOfMemory exceptions.
+    
+    ###################### MP VERSION POOL #######################
+
+    results = None
+    
+    def process_ready(r):
+        global results
+        results = r
+    
+    pool = mp.Pool(processes=PROCESSES_EVALUATION)
+    job = pool.map_async(evaluate_model,minibatches,callback=process_ready)    
+    pool.close()
+    
+    while (True):
+        if (job.ready()): break
+        print("Tasks remaining: {}".format(job._number_left*job._chunksize))
+        time.sleep(5)
+        
+    print("Tasks completed.")
+            
+    for result in results:
+        conferences.extend(result[0])
+        confidences.extend(result[1])
+        
+    model._load_model(TRAINING_DATA)
+     
+    ###################### SP VERSION ############################
+    """
+    model._load_model(TRAINING_DATA)
+
+    for index, minibatch in enumerate(minibatches,1):
+        print("Running minibatch [{}/{}]".format(index,len(minibatches)))
+        results = model.query_batch(minibatch)
+        conferences.extend(results[0])
+        confidences.extend(results[1])
+    """
+    ##############################################################
+    
+    recommendation = [conferences,confidences]
+    
+    # Evaluate.
+    
+    from EvaluationContainer import EvaluationContainer
+    evaluation = EvaluationContainer()
+    evaluation.evaluate(recommendation,truth)
